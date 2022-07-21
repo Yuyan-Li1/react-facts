83354e5944fe33dee440acb0e57eaf5b690756a3
Yuyan Li <yuyanli1211@gmail.com>
2022-07-21 23:26:26 +1000

    added png icon and modified config

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <img src="src/assets/react.svg" />
+            <img src="src/assets/reactjs-icon.png" />
             <h3 >ReactFacts</h3>
             <h4>React Course - Project 1</h4>
         </nav>
