aaeb2bf3a442fe944ac45d66c73e004b02ae4750
Yuyan Li <yuyanli1211@gmail.com>
2022-07-21 23:14:33 +1000

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <img src="src/assets/react.svg" />
+            <h3 >ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )
+}
